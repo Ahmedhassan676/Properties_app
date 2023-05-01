4eb4635689bfd7ba1c3f44ddaa0c7c037ab73e8f
Ahmedhassan676 <81989422+Ahmedhassan676@users.noreply.github.com>
2023-05-01 03:43:03 +0200

    Add files via upload

---
+++ physical_prop.py
@@ -0,0 +1,56 @@
+from thermo import Mixture
+import fluids
+import math
+import pandas as pd
+import numpy as np
+import streamlit as st
+import ht
+gases_list = ['water', 'hydrogen', 'helium', 'nitrogen', 'carbon dioxide', 'hydrogen sulfide','methane',
+'ethane', 'propane', 'isobutane', 'n-butane', 'isopentane', 'n-pentane', 'hexane',
+'heptane', 'octane', 'nonane']
+def thermo_prop(sg,t):
+        t = 1.8*t+32
+        thermo_dict = {}
+        thermal_coductivity = (0.813/sg)*(1-0.0003*(t-32))
+        cp = (1/np.sqrt(sg))*(0.388+0.00045*t)
+        cv = cp-(0.09/sg)
+        latent_heat = (1/sg)*(110.9-0.09*t)
+        thermo_dict['thermal_conductivity'] = thermal_coductivity
+        thermo_dict['Cp'] = cp
+        thermo_dict['Cv'] = cv
+        thermo_dict['latent_heat'] = latent_heat
+        return thermo_dict
+def main():
+    phases = st.multiselect('fluids presesnt',['vapor H.Cs', 'Liquid H.Cs', 'Steam', 'water', 'Gases H.Cs'])
+    if 'Gases H.Cs' in phases:
+        try:
+            # Define the pipe and conditions
+            pressure = float(st.number_input('Pressure in kg/cm2.a'))*98.0665
+            temperature_K = float(st.number_input('Temperature in C')) + 273.15
+            composition = st.multiselect('gases composition', gases_list)
+            composition_table = pd.DataFrame(index=composition,columns=['mole fraction%'])
+            
+            comp_table = st.experimental_data_editor(composition_table)
+            
+            st.write(comp_table['mole fraction%'][0])
+            mole_fractions = {comp_table.index[i]: comp_table['mole fraction%'].astype('float64')[i]/100 for i in range(len(comp_table.index))}
+            if sum(comp_table['mole fraction%'].astype('float64')) == 100:
+                # Create a gas mixture
+                st.write(mole_fractions)
+                #mole_fractions =  {"methane": 0.8, "ethane": 0.2}
+                gas_mixture = Mixture(list(mole_fractions.keys()), zs=list(mole_fractions.values()), T=temperature_K, P=pressure)
+                thermo_dict_gas = {}
+                thermo_dict_gas['thermal_conductivity'] = gas_mixture.kg
+                thermo_dict_gas['density'] = gas_mixture.rho
+                thermo_dict_gas['Cp'] = gas_mixture.Cp
+                thermo_dict_gas['visocsity'] = gas_mixture.mu
+                
+                st.write(thermo_dict_gas)
+        except IndexError: pass
+    if 'Liquid H.Cs' in phases:
+        
+        temperature = float(st.number_input('Temperature in C')) 
+        sg = float(st.number_input('Specific gravity'))
+        st.write(thermo_prop(sg,temperature))
+if __name__ == '__main__':
+    main()
