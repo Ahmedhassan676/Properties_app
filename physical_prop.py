18ababf54b7712e1c98151e0c4b63ee50ca3da19
Ahmedhassan676 <81989422+Ahmedhassan676@users.noreply.github.com>
2023-05-02 05:10:04 +0300

    Add files via upload

--- physical_prop.py
+++ physical_prop.py
@@ -4,14 +4,14 @@ import math
 import pandas as pd
 import numpy as np
 import streamlit as st
-import ht
-gases_list = ['water', 'hydrogen', 'helium', 'nitrogen', 'carbon dioxide', 'hydrogen sulfide','methane',
+
+gases_list = ['water', 'hydrogen', 'nitrogen', 'carbon dioxide', 'hydrogen sulfide','methane',
 'ethane', 'propane', 'isobutane', 'n-butane', 'isopentane', 'n-pentane', 'hexane',
 'heptane', 'octane', 'nonane']
 def thermo_prop(sg,t):
         t = 1.8*t+32
         thermo_dict = {}
-        thermal_coductivity = (0.813/sg)*(1-0.0003*(t-32)) * 0.41337910000000005
+        thermal_coductivity = (0.813/sg)*(1-0.0003*(t-32)) *0.14422790000000002
         cp = (1/np.sqrt(sg))*(0.388+0.00045*t)
         cv = cp-(0.09/sg)
         latent_heat = (1/sg)*(110.9-0.09*t) * 0.555927
@@ -48,28 +48,48 @@ def main():
                 st.write(thermo_dict_gas)
         except IndexError: pass
     if 'Liquid H.Cs' in phases:
-        temperature = float(st.number_input('Temperature in C')) 
-        sg = float(st.number_input('Specific gravity'))
-        api = (141.5/sg) - 131.5
-        if api <=14.9:
-           eta= 0.00035
-        elif api <= 34.9:
-           eta= 0.0004
-        elif api <= 50.9:
-           eta= 0.0005
-        elif api <= 63.9:
-           eta= 0.0006
-        elif api <= 78.9:
-           eta= 0.0007
-        elif api <= 88.9:
-           eta= 0.0008
-        elif api <= 93.9:
-           eta= 0.00035 
-        elif api <= 100:
-           eta= 0.0009   
-        else: st.write("please enter a valid Specific gravity") 
-        density = sg*1000*(1-eta*((temperature*1.8+32)-60))
-        st.write(thermo_prop(sg,temperature))
-        st.write("density at {} equals {} kg/m3".format(temperature, density))
+        try:
+            two_points = st.selectbox('Use 2 points of a certain property?',('No', 'Yes'), key='two_points')
+            if two_points == "Yes":
+                temperature = float(st.number_input('fluid Temperature in C', key='target_temp')) 
+                prop_menu = st.multiselect('slect property with two data points',['viscosity', 'specific gravity', 'Cp', 'thermal conductivity'])
+                temperature1 = float(st.number_input('point 1 Temperature in C', key='T1')) 
+                temperature2 = float(st.number_input('point 1 Temperature in C', key='T2')) 
+                prop_table = pd.DataFrame(index=prop_menu,columns=['point 1','point 2'])
+                prop_table_st = st.experimental_data_editor(prop_table)
+                if st.button("Reveal Calculations", key = 'calculations_tableLiq'):
+                    for i in prop_menu:
+                        
+                        if i != 'viscosity':
+                            st.write(i)
+                            A = np.array([[1,temperature1], [1,temperature2]])
+                            B = np.array([float(prop_table_st.loc[i,'point 1']),float(prop_table_st.loc[i,'point 2'])])
+                            C = np.linalg.solve(A, B)
+                            st.write(C)
+                            st.write(C[0]+temperature*C[1])
+            temperature = float(st.number_input('fluid Temperature in C', key='target_temp1'))
+            sg = float(st.number_input('Specific gravity at 15.56 C'))
+            api = (141.5/sg) - 131.5
+            if api <=14.9:
+                eta= 0.00035
+            elif api <= 34.9:
+                eta= 0.0004
+            elif api <= 50.9:
+                eta= 0.0005
+            elif api <= 63.9:
+                eta= 0.0006
+            elif api <= 78.9:
+                eta= 0.0007
+            elif api <= 88.9:
+                eta= 0.0008
+            elif api <= 93.9:
+                eta= 0.00035 
+            elif api <= 100:
+                eta= 0.0009   
+            else: st.write("please enter a valid Specific gravity") 
+            density = sg*1000*(1-eta*((temperature*1.8+32)-60))
+            st.write(thermo_prop(sg,temperature))
+            st.write("density at {} equals {} kg/m3".format(temperature, density))
+        except ZeroDivisionError: pass
 if __name__ == '__main__':
     main()
